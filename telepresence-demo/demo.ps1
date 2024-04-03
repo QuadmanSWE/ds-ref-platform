ddf26726c53aee8861e00712ca8a11cf8d7d9fb4
David Söderlund <ds@dsoderlund.consulting>
2024-04-04 00:59:45 +0200

    added a little demo script

--- telepresence-demo/demo.ps1
+++ telepresence-demo/demo.ps1
@@ -5,7 +5,12 @@ kind load docker-image tdemo:blue --name ds-ref-cluster
 docker build -t tdemo:green .
 kind load docker-image tdemo:green --name ds-ref-cluster
 
+node --inspect server.js
+
+
 t helm install
 k label namespace dsd istio-injection=disabled --overwrite
 kustomize build .\hello-k8s\ | kubectl.exe apply -f -
+
+
 t intercept hello-dev --port 3000:80
