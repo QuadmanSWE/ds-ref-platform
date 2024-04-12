155278927ea4fd4bded0b118419147876861e5a3
David Söderlund <ds@dsoderlund.consulting>
2024-04-12 08:55:34 +0200

    today is the day

--- telepresence-demo/routes/hello.js
+++ telepresence-demo/routes/hello.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 // A hello route
 router.get("/", (_req, res) => {
-  res.send("Hello World!");
+  res.send("Hello! 2024-04-12!");
 });
 
 module.exports = router;
