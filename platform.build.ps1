0ff3eff6b1a75ae992c41c9ddb4593df594ee34c
David Söderlund <ds@dsoderlund.consulting>
2024-06-22 17:59:59 +0200

    added missing dns entry for local_dns

--- platform.build.ps1
+++ platform.build.ps1
@@ -74,6 +74,7 @@ task local_dns {
 127.0.0.1 argocd.$dnsname
 127.0.0.1 pg.$dnsname
 127.0.0.1 echo.$dnsname
+127.0.0.1 grafana.$dnsname
 ############################################
 "@ | write-host
     code c:\windows\system32\drivers\etc\hosts
