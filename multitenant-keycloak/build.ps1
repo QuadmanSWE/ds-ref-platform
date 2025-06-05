6886319ecabc97803618a835c0d5b76c7dbba425
David Söderlund <ds@dsoderlund.consulting>
2025-06-05 10:43:34 +0200

    bumped to upstream keycloak 26.2.5, fixed idp-wizard hack

--- multitenant-keycloak/build.ps1
+++ multitenant-keycloak/build.ps1
@@ -1,15 +1,20 @@
+push-location multitenant-keycloak
+
+# pulls idp wizard, copies jarfile out to plugins
+7z.exe a "plugins/io.phasetwo-phasetwo-idp-wizard-0.23.jar" "theme\wizard\login\wizard.ftl"
+
 $registryName = 'docker.io'
 $registryFqdn = $registryName
 $repo = 'dsoderlund/keycloak-multitenant'
-$newtag = '20250513T151000'
-$appversion = "26.2.3"
+$newtag = get-date -format "yyyyMMddTHHmmss" #'20250513T151000'
+$appversion = "26.2.5"
 
 $localtag = "$repo`:$newtag"
 
 docker build -t $localtag --build-arg APP_SOURCE_REPO=quay.io/phasetwo/phasetwo-keycloak --build-arg APP_VERSION=$appversion .
 
 # push to kind
-kind load docker-image -n ds-ref-cluster $localtag
+# kind load docker-image -n ds-ref-cluster $localtag
 
 # ready for rock and roll
 docker login 
